07a8d395d38555efe845510fd3a3eea31b9d97bd
AmithPaavan <amithpavan796@gmail.com>
2025-05-28 13:35:35 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import './App.css';
+import axios from "axios";
+
+function App() {
+  const [query, setQuery] = useState("");
+  const [answer, setAnswer] = useState("");
+  
+  async function generateContent(){
+    setAnswer("Loading...");
+    const response = await axios({
+      url: "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyDzs8MCrDufL5L94UEv4rNsvbuBiIsdB18",
+      method: "post",
+      data: {
+        contents: [{
+          parts: [{
+            text: query
+          }]
+        }],
+      },
+    });
+    setAnswer(response["data"]["candidates"][0]["content"]["parts"][0]["text"]);
+  }
+
+  return (
+    <div className='chatbot'>
+      <h1>TalkMate A.I. Chat Bot</h1>
+      
+        <textarea placeholder='Enter query' onChange={(e)=>setQuery(e.target.value)}/>
+        <button onClick={generateContent}>Generate Answer</button>
+      
+      <pre>{answer}</pre>
+    </div>
+  );
+}
+
+export default App
