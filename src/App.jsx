adafd2954b3feafab4f719179b667f4b9d986cb9
AmithPaavan <amithpavan796@gmail.com>
2025-05-29 16:40:38 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,10 @@ function App() {
   const [answer, setAnswer] = useState("");
   
   async function generateContent(){
+    const apikey = import.meta.env.VITE_KEY;
     setAnswer("Loading...");
     const response = await axios({
-      url: "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=process.env.REACT_APP_API_KEY",
+      url: "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key="+apikey,
       method: "post",
       data: {
         contents: [{
@@ -22,13 +23,16 @@ function App() {
     setAnswer(response["data"]["candidates"][0]["content"]["parts"][0]["text"]);
   }
 
+  const handleClear = () => {
+    setAnswer([]);
+  };
+
   return (
     <div className='chatbot'>
       <h1>TalkMate A.I. Chat Bot</h1>
-      
-        <textarea placeholder='Enter query' onChange={(e)=>setQuery(e.target.value)}/>
-        <button onClick={generateContent}>Generate Answer</button>
-      
+      <textarea placeholder='Enter query' onChange={(e)=>setQuery(e.target.value)}/>
+      <button className='submit' onClick={generateContent}>Generate</button>
+      <button className='cancel' onClick={handleClear}>Clear</button>
       <pre>{answer}</pre>
     </div>
   );
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define:{
+    // eslint-disable-next-line no-undef
+    'process.env.VITE_KEY': JSON.stringify(process.env.VITE_KEY)
+  }
 })
